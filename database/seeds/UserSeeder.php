832229db01c28c003c3ec3193345e3acdbd36593
grricel <gricelbarrerab@gmail.com>
2021-11-25 22:19:25 -0500

    test users

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,6 +17,8 @@ class UserSeeder extends Seeder
     {
         $admin_rol = ParameterValue::where('name', 'Admin')->first();
         $teacher_rol = ParameterValue::where('name', 'Profesor')->first();
+        $student_rol = ParameterValue::where( 'name', 'Estudiante')->first(); 
+
         $User = [
             [
                 'first_name' => 'Admin',
@@ -33,7 +35,63 @@ class UserSeeder extends Seeder
                 'role_id' => $teacher_rol->id,
                 'email' => 'cxavier@itsa.edu.co',
                 'password' => Hash::make('secret')
-            ]
+            ],
+            [
+                'first_name' => 'Javier',
+                'first_last_name' => 'Santaolalla',
+                'photo_url' => 'https://s3.amazonaws.com/iomedia-s3/media/celebrities/10649/products/thyssen_santaolalla_marquee__L.jpeg',
+                'role_id' => $teacher_rol->id,
+                'email' => 'jsantaolalla@itsa.edu.co',
+                'password' => Hash::make('secret')
+            ],
+            [
+                'first_name' => 'Selena',
+                'first_last_name' => 'Gomez',
+                'photo_url' => 'https://e.radio-grpp.io/normal/2018/09/05/325532_673318.jpg',
+                'role_id' => $teacher_rol->id,
+                'email' => 'sgomez@itsa.edu.co',
+                'password' => Hash::make('secret')
+            ],
+            [
+                'first_name' => 'Susana',
+                'first_last_name' => 'Rodriguez',
+                'photo_url' => 'https://www.show.news/__export/1612130998908/sites/debate/img/2021/01/31/emmaa_crop1612130950724.png_943222218.png',
+                'role_id' => $teacher_rol->id,
+                'email' => 'srodriguez@itsa.edu.co',
+                'password' => Hash::make('secret')
+            ], 
+            [
+                'first_name' => 'Juan',
+                'first_last_name' => 'Anachury',
+                'photo_url' => 'https://wl-genial.cf.tsp.li/resize/728x/jpg/fb1/d1b/28cce45b038d6baddba3db025d.jpg',
+                'role_id' => $student_rol->id,
+                'email' => 'juanachury@itsa.edu.co',
+                'password' => Hash::make('secret')
+            ],
+            [
+                'first_name' => 'Valerie',
+                'first_last_name' => 'Cordero',
+                'photo_url' => 'https://media.istockphoto.com/photos/facing-my-future-with-confidence-picture-id1139495117?k=20&m=1139495117&s=612x612&w=0&h=6P1Q43Oi_yERh9pMLki5LrkF3jaG82puPMeQdCoUNx8=',
+                'role_id' => $student_rol->id,
+                'email' => 'vcordero@itsa.edu.co',
+                'password' => Hash::make('secret')
+            ], 
+            [
+                'first_name' => 'Jesus',
+                'first_last_name' => 'Ballesteros',
+                'photo_url' => 'https://www.studentworldonline.com/userfiles/images/Students/Vietnamese%20student_istock.jpg.jpg',
+                'role_id' => $student_rol->id,
+                'email' => 'jballesteros@itsa.edu.co',
+                'password' => Hash::make('secret')
+            ],
+            [
+                'first_name' => 'Sielva',
+                'first_last_name' => 'Maldonado',
+                'photo_url' => 'https://www.itsmiparis.com/wp-content/themes/nextline_v4/images/itsmi_student_life.jpg',
+                'role_id' => $student_rol->id,
+                'email' => 'smaldonado@itsa.edu.co',
+                'password' => Hash::make('secret')
+            ],
         ];
 
         foreach ($User as  $user) {
