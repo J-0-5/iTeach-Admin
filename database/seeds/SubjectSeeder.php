b47b9a798d3d0ae8743ee7d57039dfb32c8a7564
Jos <jose20010822@gmail.com>
2021-11-27 01:15:47 -0500

    requestTutoring

--- database/seeds/SubjectSeeder.php
+++ database/seeds/SubjectSeeder.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Parameter;
-use App\ParameterValue;
 use App\Subject;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class SubjectSeeder extends Seeder
 {
@@ -16,28 +13,27 @@ class SubjectSeeder extends Seeder
     public function run()
     {
         $Subjects = [
-                'Administración de servidores',
+            'Administración de servidores',
 
-                'Administración de SO', 
+            'Administración de SO',
 
-                'Diseño y desarrollode Aplicaciones móviles',
+            'Diseño y desarrollode Aplicaciones móviles',
 
-                'Programación con Frameworks para Web',
+            'Programación con Frameworks para Web',
 
-                'Desarrollo Humano',
-                
-                'Álgebra lineal',
-                
-                'Cálculo II',
-                
-                'Inglés',
-               
-        ]; 
+            'Desarrollo Humano',
+
+            'Álgebra lineal',
+
+            'Cálculo II',
+
+            'Inglés',
+
+        ];
         foreach ($Subjects as $Subject) {
             Subject::create([
                 'name' => $Subject,
             ]);
         }
-
     }
 }
